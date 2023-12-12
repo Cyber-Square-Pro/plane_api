14070f5d073936731be9ceac2d3653e7c33238a5
Rifad <140198464+MohammedRifadCyberSquare@users.noreply.github.com>
2023-12-12 16:44:53 +0400

    Update local.py

--- plane/settings/local.py
+++ plane/settings/local.py
@@ -23,10 +23,10 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "plane",
+        "NAME": "plane_db",
         "USER": "postgres",
-        "PASSWORD": "root",
-        "HOST": "localhost",
+        
+        "HOST": "127.0.0.1",
     }
 }
 
