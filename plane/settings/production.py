6d8eb84ff1d87fde9dfbdaef5311f751847d5f82
Rifad <140198464+MohammedRifadCyberSquare@users.noreply.github.com>
2023-12-12 17:04:10 +0400

    Update production.py

--- plane/settings/production.py
+++ plane/settings/production.py
@@ -20,7 +20,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "plane_db",
         "USER": "cybersquare_pro",
-        "PASSWORD":"cs-pro@123cybersquare_pro",
+        "PASSWORD":"cs-pro@123",
         "HOST": "127.0.0.1",
     }
 }
