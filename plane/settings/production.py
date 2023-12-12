49c075c41d443d40e03be919e55943d18b5821aa
Rifad <140198464+MohammedRifadCyberSquare@users.noreply.github.com>
2023-12-12 16:44:26 +0400

    production_db_connection updated

--- plane/settings/production.py
+++ plane/settings/production.py
@@ -18,10 +18,10 @@ DEBUG = True
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "plane",
+        "NAME": "plane_db",
         "USER": "postgres",
-        "PASSWORD": "root",
-        "HOST": "localhost",
+        
+        "HOST": "127.0.0.1",
     }
 }
 
