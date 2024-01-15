d2ae6fdd46ffd7ce095a63b41326dfdb5003d935
Rifad <140198464+MohammedRifadCyberSquare@users.noreply.github.com>
2024-01-15 19:01:04 +0400

    db name changed

--- plane/settings/local.py
+++ plane/settings/local.py
@@ -23,7 +23,7 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "plane_db",
+        "NAME": "plane_db_test",
         "USER": "postgres",
         "PASSWORD":"root@cybersquare",
         "HOST": "127.0.0.1",
